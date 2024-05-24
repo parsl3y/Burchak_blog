e98585db8b7ca40b60285a2b27e489bb6d0a774d
parsl3y <burchakpetro@gmail.com>
2024-05-24 10:10:08 +0300

    Feat: add migration to bd. Create tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,10 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call(UsersTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
+        \App\Models\BlogPost::factory(100)->create();
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
