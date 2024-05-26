29a4b4330f4f5e03d14de81f86bb86c3fe9e8fa9
parsl3y <burchakpetro@gmail.com>
2024-05-26 10:22:38 +0300

    Feat: Complete lab 5. Add function Create

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers\Blog\Admin;
 /*use App\Http\Controllers\Controller;*/
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
 
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
 class CategoryController extends BaseController
 {
     /**
@@ -24,16 +25,34 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        /*  dd(__METHOD__);*/
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        /*  dd(__METHOD__);*/
-    }
+       $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+$item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
+
+if ($item) {
+    return redirect()
+        ->route('blog.admin.categories.edit', [$item->id])
+        ->with(['success' => 'Успішно збережено']);
+} else {
+    return back()
+        ->withErrors(['msg' => 'Помилка збереження'])
+        ->withInput();
+}
+}
 
     /**
      * Display the specified resource.
@@ -57,7 +76,7 @@ class CategoryController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
         $item = BlogCategory::find($id);
         if (empty($item)) { //якщо ід не знайдено
